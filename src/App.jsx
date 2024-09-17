34662c053a6804614a5063930ed34e1ea3435389
Willem Houm <willem.houm@golem.ai>
2024-09-17 09:56:22 +0200

    sidebar advancing

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
-import React, { useState } from 'react';
-import Home from './components/Home/Home';
-import TicTacToe from './components/TicTacToe/TicTacToe';
-import Sidebar from './components/Sidebar/Sidebar';
+import React, { useState } from "react";
+import Home from "./components/Home/Home";
+import TicTacToe from "./components/TicTacToe/TicTacToe";
+import Sidebar from "./components/Sidebar/Sidebar";
 
 function App() {
-  const [currentView, setCurrentView] = useState('tictactoe');
-  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const [currentView, setCurrentView] = useState("tictactoe");
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
 
   const renderView = () => {
     switch (currentView) {
-      case 'home':
+      case "home":
         return <Home />;
-      case 'tictactoe':
+      case "tictactoe":
         return <TicTacToe />;
       default:
         return <Home />;
@@ -21,7 +21,13 @@ function App() {
   return (
     <>
       <div className="view-container">
-        <Sidebar setCurrentView={setCurrentView} isOpen={isSidebarOpen}/>
+        <Sidebar
+          setCurrentView={setCurrentView}
+          isOpen={isSidebarOpen}
+          switchMenuState={() => {
+            setIsSidebarOpen(!isSidebarOpen);
+          }}
+        />
         {renderView()}
       </div>
     </>
--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,30 +1,64 @@
-import './Sidebar.css';
-import menuItems from '../../assets/menu-items.json';
+import React, { useState, useEffect } from "react";
+import "./Sidebar.css";
+import menuItems from "../../assets/menu-items.json";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHome, faGamepad, faBars } from "@fortawesome/free-solid-svg-icons";
 
-function Sidebar({ setCurrentView, isOpen }) {
-    console.log(menuItems);
+const icons = {
+  faHome,
+  faGamepad,
+};
 
-    if (!isOpen) {
-        return (
-            <aside className={`menu folded`}>
-                {menuItems.map((item, index) => (
-                    <div className="menuElement" key={index} onClick={() => setCurrentView(item.view)}>
-                        <img className='icon' src={item.icon} />
-                    </div>
-                ))}
-            </aside>
-        );
-    } else {
-        return (
-            <aside className={`menu unfolded`}>
-                {menuItems.map((item, index) => (
-                    <div className="menuElement" key={index} onClick={() => setCurrentView(item.view)}>
-                        <img className='icon' src={item.icon} />
-                    </div>
-                ))}
-            </aside>
-        );
-    }
+function Sidebar({ setCurrentView, isOpen, switchMenuState }) {
+  const handleMenuClick = () => {
+    switchMenuState();
+  };
+  if (!isOpen) {
+    return (
+      <aside className={`menu folded`}>
+        <div
+          className="menuElement"
+          onClick={() => {
+            handleMenuClick();
+          }}
+        >
+          <FontAwesomeIcon className="icon" icon={faBars} />
+        </div>
+        {menuItems.map((item, index) => (
+          <div
+            className="menuElement"
+            key={index}
+            onClick={() => setCurrentView(item.view)}
+          >
+            <FontAwesomeIcon className="icon" icon={icons[item.icon]} />
+          </div>
+        ))}
+      </aside>
+    );
+  } else {
+    return (
+      <aside className={`menu unfolded`}>
+        <div
+          className="menuElement"
+          onClick={() => {
+            handleMenuClick();
+          }}
+        >
+          <FontAwesomeIcon className="icon" icon={faBars} />
+        </div>
+        {menuItems.map((item, index) => (
+          <div
+            className="menuElement"
+            key={index}
+            onClick={() => setCurrentView(item.view)}
+          >
+            <FontAwesomeIcon className="icon" icon={icons[item.icon]} />
+            <div className="icon-name">{item.name}</div>
+          </div>
+        ))}
+      </aside>
+    );
+  }
 }
 
 export default Sidebar;
