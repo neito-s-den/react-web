9ca4428a4250b2a6e4146fc374152a872a44664f
Willem Houm <willem.houm@golem.ai>
2024-09-17 23:14:12 +0200

    added on key press for addplayer

--- src/components/NoNameCardGame/PlayerManager.jsx
+++ src/components/NoNameCardGame/PlayerManager.jsx
@@ -1,45 +1,70 @@
-import React, { useState } from 'react';
-import styles from './PlayerManager.module.css';
+import React, { useState } from "react";
+import styles from "./PlayerManager.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTrash } from "@fortawesome/free-solid-svg-icons";
 
 function PlayerManager({ onAddPlayerClicked, onRemovePlayerClicked, players }) {
-    const [player, setPlayer] = useState("");
+  const [player, setPlayer] = useState("");
 
-    const addPlayer = () => {
-        console.log("Adding player...");
-        !players.includes(player) ? onAddPlayerClicked(player) : alert('Player already exists');
-        setPlayer("");
+  const addPlayer = () => {
+    console.log("Adding player...");
+    !players.includes(player)
+      ? onAddPlayerClicked(player)
+      : alert("Player already exists");
+    setPlayer("");
+  };
+
+  const removePlayer = (index) => {
+    console.log("Removing index...", index);
+    onRemovePlayerClicked(player);
+  };
+
+  const handleInputChange = (event) => {
+    setPlayer(event.target.value);
+  };
+
+  const handleKeyPress = (event) => {
+    if (event.key === "Enter") {
+      console.log("Enter key pressed");
+
+      addPlayer();
     }
+  };
 
-    const removePlayer = (index) => {
-        console.log("Removing index...", index);
-        onRemovePlayerClicked(player);
-    };
-
-    const handleInputChange = (event) => {
-        setPlayer(event.target.value);
-    };
-
-    return (
-        <div className={styles.root}>
-            <div className={styles.nameInput}>
-                <input type="text" name="newPlayer" id="newPlayer" placeholder='New player name...' onChange={handleInputChange} value={player} />
-                <button onClick={addPlayer} disabled={!player}>Add player</button>
+  return (
+    <div className={styles.root}>
+      <div className={styles.nameInput}>
+        <input
+          type="text"
+          name="newPlayer"
+          id="newPlayer"
+          placeholder="New player name..."
+          onChange={handleInputChange}
+          onKeyUp={handleKeyPress}
+          value={player}
+        />
+        <button onClick={addPlayer} disabled={!player}>
+          Add player
+        </button>
+      </div>
+      <div className={styles.title}>Players List</div>
+      <div className={styles.playerList}>
+        {players.map((player, index) => (
+          <div key={index} className={styles.player}>
+            <div>{player}</div>
+            <div className={styles.trashIcon}>
+              <FontAwesomeIcon
+                icon={faTrash}
+                onClick={() => {
+                  removePlayer(index);
+                }}
+              />
             </div>
-            <div className={styles.title}>Players List</div>
-            <div className={styles.playerList}>
-                {players.map((player, index) => (
-                    <div key={index} className={styles.player}>
-                        <div>{player}</div>
-                        <div className={styles.trashIcon}>
-                            <FontAwesomeIcon icon={faTrash} onClick={() => {removePlayer(index)}}/>
-                        </div>
-                    </div>
-                ))}
-            </div>
-        </div>
-    )
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default PlayerManager;
